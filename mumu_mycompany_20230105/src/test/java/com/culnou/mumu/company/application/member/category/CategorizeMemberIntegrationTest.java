3c656c7b86fdd912c93e725aea8ecc68cf127054
sagawa <sagawa_satoshi@culnou.com>
2023-01-06 20:10:59 +0900

    Jenkins Test2

---
+++ mumu_mycompany_20230105/src/test/java/com/culnou/mumu/company/application/member/category/CategorizeMemberIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.culnou.mumu.company.application.member.category;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CategorizeMemberIntegrationTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		System.out.println("***** 統合テスト *****");
+		System.out.println("***** メンバーを分類する。*****");
+	}
+
+}
---
+++ mumu_mycompany_20230105/src/test/java/com/culnou/mumu/company/ui/member/category/CategorizeMemberSystemTest.java
@@ -0,0 +1,26 @@
+package com.culnou.mumu.company.ui.member.category;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CategorizeMemberSystemTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		System.out.println("***** システムテスト *****");
+		System.out.println("***** メンバーを分類する。*****");
+	}
+
+}
